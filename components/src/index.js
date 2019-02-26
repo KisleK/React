aeeb02684ea5347d9ec40320ff5fe08c90c8cd29
KisleK <kisle.kuhn1@gmail.com>
2019-02-26 12:59:27 -0600

    component nesting easy and hard

--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Long way!!!!
+/*
+Created main div with comments inside, copy and past, then change name and date and post
+this takes a long time to create and update
+why not make re-usable components
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+*/
+const App =() => {
+    return (// any time you want to put a big block of JSX use ()
+        <div className="ui container comments">
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
+            <CommentDetail/>
+        </div>
+    );
+};
+
+
+ReactDom.render(<App/>, document.querySelector('#root'));
