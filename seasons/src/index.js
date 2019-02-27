2b9fa443079dc350ba79efe163da7a1df3a8534a
KisleK <kisle.kuhn1@gmail.com>
2019-02-27 13:50:47 -0600

    initialized state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+class App extends React.Component {
+    // not required by react, but is a function that belongs to JS language
+    // constructor function is the first function in a JS class, any time an instance of this class is created. (new instances of the APP class)
+    //constructor function is a good place to initialize state
+   constructor (props) {
+       super(props);
+
+       this.state = { lat: null };  //this is the state object
+
+   }
+
+    // React says we have to define Render!!
+    // must use render any time we make a class based component
+    render () {
+        window.navigator.geolocation.getCurrentPosition (
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return <div>Latitude: {this.state.lat} </div>
+    }
+}
+
+
+ReactDom.render (
+  <App/>,
+  document.querySelector('#root')
+);
