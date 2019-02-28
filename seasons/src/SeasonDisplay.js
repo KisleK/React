e48307f655b9abe9e91cd4688a2ac75d3d9a8e4d
KisleK <kisle.kuhn1@gmail.com>
2019-02-28 11:10:45 -0600

    created load screen

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,46 @@
+import './SeasonDisplay.css'
 import React from 'react';
+//this const is a refactor of the ternary expressions
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Brrrrrr it\'s chilly!',
+        iconName: 'snowflake'
+    }
+};
 
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    //if lat greater than 0 return summer otherwise return winter
+    }   else {
+        return lat > 0 ? 'winter' : 'summer';
+    //if lat greater than 0 return winter otherwise return summer
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>
+}
+
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+
+//removed these ternary expressions because of refactor on line 3
+    //const text = season === 'winter' ? "Brrrrr it's chilly" : "Lets hit the beach!";
+    //const icon = season === 'winter' ? 'snowflake' : 'sun';
+
+    const { text, iconName } = seasonConfig[season]; //will return {text, iconName}
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+                <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
+
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
 
 
 
 
 class App extends React.Component {
-    // not required by react, but is a function that belongs to JS language
+
+
+    // constructor not required by react, but is a function that belongs to JS language
     // constructor function is the first function in a JS class, any time an instance of this class is created. (new instances of the APP class)
     //constructor function is a good place to initialize state
-   constructor (props) {
-       super(props);
+   //constructor (props) {
+    //   super(props); // must call super or will throw an error
+       //this.state = { lat: null, errMessage: '' };  //This is one way to initialize state //this is the state object THE ONLY TIME WE EVER DO A DIRECT ASSIGNMENT TO this.state
+//}
 
-       this.state = { lat: null };  //this is the state object
+state = { lat: null, errMessage: '' };// this is the refactored "other" way to initialize state // When doing this you do not need a constructor function or to call super
 
-   }
+    componentDidMount(){ //must be written outside of constructor
+
+
+        window.navigator.geolocation.getCurrentPosition (
+            (position) => this.setState({ lat: position.coords.latitude }),
+                // WE DID NOT DO A DIRECT ASSIGNMENT
+                // THIS IS A DIRECT ASSIGNMENT      this.state ({ lat: position.coords.latitude });
+            (err) => this.setState({ errMessage: err.message  })
+        );
+    }
+
 
     // React says we have to define Render!!
     // must use render any time we make a class based component
     render () {
-        window.navigator.geolocation.getCurrentPosition (
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-        return <div>Latitude: {this.state.lat} </div>
+        if (this.state.errMessage && !this.state.lat) {
+            return <div>Error: {this.state.errMessage}</div>;
+        }
+        if (!this.state.errMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>;
+        }
+        return <Spinner message="Please accept location request"/>;
     }
 }
 
